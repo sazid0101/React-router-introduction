de655c467ad5a6317d2b490dd271359c41cbd8a9
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-10 13:58:43 +0600

    error component added

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+    return (
+        <div>
+            <h3>Oooooops..........!</h3>
+        </div>
+    );
+};
+
+export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -14,11 +14,13 @@ import Users from './Components/Users/Users.jsx';
 import UserDetails from './Components/UserDetails/UserDetails.jsx';
 import Posts from './Components/Posts/Posts.jsx';
 import PostDetails from './Components/PostDetails/PostDetails.jsx';
+import Error from './Components/Error/Error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: '/about',
