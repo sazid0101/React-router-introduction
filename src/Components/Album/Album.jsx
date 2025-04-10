654e46e6def9cffb6faa20711725911378627e56
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-10 17:59:01 +0600

    ...

--- src/Components/Album/Album.jsx
+++ src/Components/Album/Album.jsx
@@ -1,7 +1,12 @@
 const Album = ({album}) => {
     const {id,title}= album
+
+    const postStyle={
+        border: '2px solid green',
+       margin:'3px'
+   }
     return (
-        <div>
+        <div style={postStyle}>
             <h4>Album</h4>
             <p>{id}</p>
             <p>{title}</p>
--- src/Components/Albums/Albums.jsx
+++ src/Components/Albums/Albums.jsx
@@ -2,13 +2,20 @@ import { useLoaderData } from "react-router-dom";
 import Album from "../Album/Album";
 const Albums = () => {
     const loadAlbums = useLoaderData()
+
+    const albumsStyle={
+        display:'grid',
+        gridTemplateColumns: 'repeat(4, auto)',
+    }
     return (
         <div>
             <h3>Albums</h3>
             <p>{loadAlbums.length}</p>
+            <div style={albumsStyle}>
             {
                 loadAlbums.map((album,index)=> <Album key={index} album={album}></Album>)
             }
+            </div>
         </div>
     );
 };
